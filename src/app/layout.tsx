f83711f32ea8fc30c4f88916fbcce7f233d302e9
suleyman nurhussen <suleymanerziku@gmail.com>
2025-06-24 15:21:33 +0000

    Error: In HTML, <head> cannot be a child of <body>. This will cause a hy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   // The main layout logic, including providers, is in `src/app/[locale]/layout.tsx`.
   return (
     <html lang="en" suppressHydrationWarning>
+       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
+      </head>
       <body className="font-body">{children}</body>
     </html>
   );
